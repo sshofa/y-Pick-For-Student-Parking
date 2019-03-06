bc5ffbdf16c7e4a1ef8ec477cc508d0d0abaa3d4
sshofa <44119869+sshofa@users.noreply.github.com>
2019-03-05 21:34:46 -0800

    Adding simulations to see if the probability converges

--- LotteryForStudentParking.py
+++ LotteryForStudentParking.py
@@ -1,20 +1,26 @@
 import random
 
-def pickStudent(numStudent, numTrials):
+def pickStudent(numStudent, numPicks):
     #apply weights to certain students. i.e. imagine that students 0-10 bought 7 tickets
-    studentNumber = 7*list(range(0,10)) + 5*list(range(11,30)) + 3*list(range(31,333)) + list(range(334,449))
+    studentNumber = 100*list(range(0,4)) + 3*list(range(4,30)) + list(range(31,333))
     result = []
     numHits = 0
-    for t in range(numTrials):
+    for t in range(numPicks):
         chosenStudent = random.choice(studentNumber)
         result.append(chosenStudent)
         studentNumber.remove(chosenStudent)
         if chosenStudent == numStudent:
             numHits += 1
-        probability = round((numHits / numTrials)*100,3)
-    print('The probability that I will get picked is ', probability,'%')
+        probability = round((numHits / numPicks)*100,3)
+    return probability
 
-pickStudent(3, 50)
+def simulate(numStudent, numPicks, numTrials):
+    for t in range(numTrials):
+        probability = []
+        probability.append(pickStudent(numStudent, numPicks))
+        true_probability = sum(probability)/len(probability)
+    print('The probability that I will get picked is ', true_probability,'%')
 
-
-
+for simulations in (1000, 10000, 100000, 1000000, 10000000):
+    print("Simulation iterations: ", simulations)
+    simulate(3, 100, simulations)
